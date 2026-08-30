b5aab1bbe4bcf1748fd048109d53f1323fcbe9c5
Faizan Munir <faizanmunir8774@gmail.com>
2026-08-31 00:08:34 +0500

    Connect to local MySQL with DB_ env variables

--- index.js
+++ index.js
@@ -17,14 +17,11 @@ const JWT_SECRET = process.env.JWT_SECRET;
 // MySQL Connection
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
